8f080fa58c1978b5421edecb8dbea9e9bb0600bb
tanmay.gondavale-at-243268985733 <tanmay.gondavale-at-243268985733>
2021-03-15 08:13:17 +0530

    UserMS 15 march

---
+++ src/test/java/com/infy/UserMS/UserMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.UserMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserMsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
